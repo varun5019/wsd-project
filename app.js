8ca55e72b4e8b1ac3a6451e4d819829e380b4e89
Varun Sangwan <74721898+varun5019@users.noreply.github.com>
2025-04-29 14:43:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ import { Hono } from "jsr:@hono/hono@4.6.5";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello World!"));
+app.get("/", (c) => c.text("Hi there!"));
 
 export default app;
